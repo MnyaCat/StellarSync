97ee0d41a5bd128e3df96452b5513fd7c09af555
MnyaCat <mnnk.1314@gmail.com>
2025-03-19 00:34:30 +0900

    rename: メインクラスの名前を変更

--- paper/src/main/kotlin/dev/mnyacat/stellar_sync_paper/StellarSyncPaper.kt
+++ paper/src/main/kotlin/dev/mnyacat/stellar_sync_paper/StellarSyncPaper.kt
@@ -2,7 +2,7 @@ package dev.mnyacat.stellar_sync_paper
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class Stellar_sync_paper : JavaPlugin() {
+class StellarSyncPaper : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
