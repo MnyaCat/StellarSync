52c7c7e38f1bc56ae3a176434861e6aa742ec4cf
MnyaCat <mnnk.1314@gmail.com>
2025-03-26 22:20:30 +0900

    delete: 不要なif文を削除

--- fabric/src/main/java/dev/mnyacat/stellar_sync_fabric/mixin/PlayerManagerMixin.java
+++ fabric/src/main/java/dev/mnyacat/stellar_sync_fabric/mixin/PlayerManagerMixin.java
@@ -1,13 +1,10 @@
 package dev.mnyacat.stellar_sync_fabric.mixin;
 
-import dev.mnyacat.stellar_sync_fabric.model.FabricGlobalContext;
 import dev.mnyacat.stellar_sync_fabric.model.FabricStorageContext;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ConnectedClientData;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,10 +16,6 @@ public class PlayerManagerMixin {
     @Inject(method = "onPlayerConnect", at = @At("TAIL"))
     private void onPlayerJoin(ClientConnection connection, ServerPlayerEntity player, ConnectedClientData clientData, CallbackInfo ci) {
         // 接続時のロード処理
-        if (!FabricGlobalContext.INSTANCE.getPluginEnable()) {
-            player.sendMessage(Text.literal("StellarSyncが無効化されているため, プレイヤーデータの同期が行われません: サーバー管理者に問い合わせてください.").formatted(Formatting.RED));
-            return;
-        }
         FabricStorageContext.INSTANCE.getStorageWrapper().loadPlayerData(player, true);
 
     }
