4905ed6a6503697c91ef6287791a448061a74dfa
MnyaCat <mnnk.1314@gmail.com>
2025-03-26 21:40:52 +0900

    add: PaperのContextを実装

---
+++ paper/src/main/kotlin/dev/mnyacat/stellar_sync_paper/model/PaperGlobalContext.kt
@@ -0,0 +1,6 @@
+package dev.mnyacat.stellar_sync_paper.model
+
+import dev.mnyacat.stellar_sync_common.model.GlobalContext
+import net.kyori.adventure.text.Component
+
+object PaperGlobalContext: GlobalContext<Component>()
